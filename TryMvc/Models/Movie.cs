fc815def5c5196de61801189c290932c1a4fc486
diegomoreira92 <diegomoreiraalves.92@gmail.com>
2015-02-10 16:42:37 -0200

    Finish class 7 - http://tiny.cc/rauutx
    - Explain the generated code
    - Add globalize.js library to fix date problem in Others languages

--- TryMvc/Models/Movie.cs
+++ TryMvc/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 
 namespace TryMvc.Models
@@ -7,12 +8,18 @@ namespace TryMvc.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+
+        [Display(Name= "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
     }
 
-    public class MovieDBContext : DbContext { 
+    public class MovieDBContext : DbContext
+    { 
         public DbSet<Movie> Movies {get; set;}
     }
 }
