6d3421033be907785df0ef654999e7a99f413352
diegomoreira92 <diegomoreiraalves.92@gmail.com>
2015-02-11 18:43:57 -0200

    Finish class 9 - http://tiny.cc/6juwtx
    - Migrations
    - Update Models
    - Add a new fields

--- TryMvc/Controllers/MoviesController.cs
+++ TryMvc/Controllers/MoviesController.cs
@@ -64,7 +64,7 @@ namespace TryMvc.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include="ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public ActionResult Create([Bind(Include="ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
@@ -96,7 +96,7 @@ namespace TryMvc.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include="ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
